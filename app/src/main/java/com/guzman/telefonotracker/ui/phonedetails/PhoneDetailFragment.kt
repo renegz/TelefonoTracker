a185685e30e89b8e0996797268c0c2e413e645c5
renegz <00077321@uca.edu.sv>
2023-05-23 10:26:15 -0600

    Add: On Click item functionality

--- app/src/main/java/com/guzman/telefonotracker/ui/phonedetails/PhoneDetailFragment.kt
+++ app/src/main/java/com/guzman/telefonotracker/ui/phonedetails/PhoneDetailFragment.kt
@@ -5,16 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.guzman.telefonotracker.R
+import com.guzman.telefonotracker.databinding.FragmentPhoneDetailBinding
+import com.guzman.telefonotracker.ui.viewmodel.PhoneViewModel
 
 class PhoneDetailFragment : Fragment() {
+
+    private val viewModel: PhoneViewModel by activityViewModels {
+        PhoneViewModel.Factory
+    }
+
+    private lateinit var binding: FragmentPhoneDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_phone_detail, container, false)
+        binding = FragmentPhoneDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setViewModel()
+    }
+
+    private fun setViewModel(){
+        binding.viewmodel = viewModel
     }
 
 }
